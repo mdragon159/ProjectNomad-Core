ac95e3e23c165fb0c1a9580d1918c9e1dceb5798
Jawad <jawadmn@gmail.com>
2022-09-21 17:43:34 -0400

    Add MoveDataProvider to SimContext... and realize SimContext needs to be removed from this project
    Yay repo commit process forcing me to realize this file is actually in the core project- when an include is
    actually in the SimLayer project.
    
    Barely getting away with this due to lack of unit tests and former project include setup in actual game.
    Will refactor/rework in future... eventually!

--- ProjectNomadCore/Context/SimContext.h
+++ ProjectNomadCore/Context/SimContext.h
@@ -1,15 +1,23 @@
 #pragma once
+
 #include "SimFrame.h"
+#include "DataProviders/MoveDataProvider.h"
 #include "EnTT/entt.hpp"
 #include "Utilities/LoggerSingleton.h"
 #include "Utilities/Singleton.h"
 
 namespace ProjectNomad {
+    // TODO: "Circular" project dependencies,rRefactor SimContext out of Core project! Perhaps CoreContext template/base type?
     struct SimContext {
         SimFrame simFrame = {};
         entt::registry registry = {};
 
-        // References so don't necessary need to directly get these everywhere
+        // Store singleton references so not necessary to directly ::get() everywhere
         LoggerSingleton& logger = Singleton<LoggerSingleton>::get();
+
+        // Save effort passing around objects to systems by stuffing them into the context object.
+        // Note that these objects are expected to be freely used by systems. Thus, not adding certain objects which
+        // need to be more careful with (eg, render interface).
+        MoveDataProvider moveDataProvider;
     };
 }
