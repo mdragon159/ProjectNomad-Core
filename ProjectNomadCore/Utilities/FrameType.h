6a43af7af78e617719a52d10bca2303abc28f5fb
Jawad <jawadmn@gmail.com>
2022-09-23 04:46:52 -0400

    Minor: Add explicit type include for uint32_t
    Keep forgetting to add this for certain build tests. When things fail due to this, things REALLY fail
    surprisingly!

--- ProjectNomadCore/Utilities/FrameType.h
+++ ProjectNomadCore/Utilities/FrameType.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 namespace ProjectNomad {
     // Simple alias which allows both easy changing of frame type in one place as well as clarity as to what the type is
     //  intended to signify
